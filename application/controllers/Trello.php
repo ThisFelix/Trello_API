f193244d1f9ce258282afe0d41b97d074952fbe4
ThisFelix <matheus.felix96@hotmail.com>
2017-06-25 17:10:43 -0300

    Update Trello.php

--- application/controllers/Trello.php
+++ application/controllers/Trello.php
@@ -1,11 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// a criação de um controlador pode ser entendida
-// como a criação de uma seção no seu site...
-// pense em cada função como uma subseção
-
-// um controlador é um gerador de páginas
 class Trello extends CI_Controller {
 	
 	function index(){
@@ -24,11 +19,12 @@ class Trello extends CI_Controller {
 	function logs(){
 		$this->load->model('My_Model', 'model');
 		$dados['dados'] = $this->model->lista('id', 'asc');
-		$html = $this->load->view('logs', $dados, true);
+		$html = '<body>';
+		$html .= $this->load->view('logs', $dados, true);
 		$this->show($html);
 	}
 			
-	// copiar e colar não é coisa de gente decente
+	
 	function show($content){
 		$html = $this->load->view('common/header', null, true);
 		$html .= $this->load->view('common/navbar', null, true);
