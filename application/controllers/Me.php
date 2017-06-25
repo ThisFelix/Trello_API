ff3d0317fd3ddb4b495ac7458ba921413422c9e2
ThisFelix <matheus.felix96@hotmail.com>
2017-06-25 17:11:22 -0300

    Update Me.php

--- application/controllers/Me.php
+++ application/controllers/Me.php
@@ -3,12 +3,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Class Me extends CI_Controller{
 	
 	function index(){
-		$html = $this->load->view('Index_view', null, true);
+		$html = '<body>';
+		$html .= $this->load->view('Index_view', null, true);
 		$this->show($html);
 	}
 	
 	function informacoes(){
-		$html = $this->load->view('inform_view', null, true);
+		$html = '<body>';
+		$html .= $this->load->view('inform_view', null, true);
 		$this->show($html);
 	}
 	
